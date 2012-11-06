043f7fd9c222e64a437ae1da2e062a47712b2ab2
youngsterxyf <sas.198708@gmail.com>
2012-11-06 13:33:51 +0800

    将config.py作为配置文件，所有piliangDownloader.py中需要的配置信息都在config.py中设定

---
+++ config.py
@@ -0,0 +1,10 @@
+#coding: utf-8
+
+# 歌手姓名列表
+singerList = ['叶蓓', '金莎']
+
+# 音乐存储目录
+musicDir = '~/Music'
+
+# 是否按照专辑下载
+album = True
--- piliangDownloader.py
+++ piliangDownloader.py
@@ -3,21 +3,17 @@
 
 import os
 import gevent
+import config
 
-#singerList = ["任贤齐", "罗大佑", "朴树", "陈奕迅", "方大同", "周杰伦", "曲婉婷", "莫文蔚", "孙燕姿"]
-singerList = ["曹格", "张震岳"]
 cmdList = list()
 
-# 如果想下载歌手的所有专辑，而不是单曲，则设为True
-album = True
+optionalArgs = ' -d ' + config.musicDir + ' &'
 
-optionalArgs = ' -d ~/Music &'
-if album:
+if config.album:
 	optionalArgs = ' -a' + optionalArgs
 
-for singer in singerList:
-	cmdList.append('./getBaiduMusic.py ' + singer + optionalArgs)
+for singer in config.singerList:
+	cmdList.append('python getBaiduMusic.py ' + singer + optionalArgs)
 
 threads = [gevent.spawn(os.system, task) for task in cmdList]
-print len(threads)
 gevent.joinall(threads)
