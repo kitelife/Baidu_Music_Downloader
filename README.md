a718f7c861fbe6f5edf6035cda2d5f5a1a1f2706
youngsterxyf <sas.198708@gmail.com>
2012-11-26 14:11:56 +0800

    添加单曲下载支持

--- README.md
+++ README.md
@@ -12,10 +12,15 @@
 
 - 命令行输入：python getBaiduMusic.py 歌手姓名 [-a] [-d 音乐存储目录]
 
-其中[]内的选项是可选的，歌手姓名是必须的，且必须紧接getBaiduMusic.py。选项-a是指定按专辑下载歌手的所有音乐，如果没有-a选项，则下载所有单曲
+其中方括号内的选项是可选的，歌手姓名是必须的，且必须紧接getBaiduMusic.py。选项-a是指定按专辑下载歌手的所有音乐，如果没有-a选项，则下载所有单曲
 
 - 如果想同时下载多个歌手的音乐，则可以先在文件config.py设定相关配置信息，然后执行：python piliangDownloader.py
 
 **使用方法(Windows下)**:
 
 与Linux下的使用方法一致，只是需要注意设定配置文件config.py中musicDir一项的值，默认值为linux下用户home目录中Music目录(~/Music)
+
+
+**更新**
+
+- 2012-11-26：支持单曲下载--- python getBaiduMusic.py -m 歌曲名 [-d 音乐存储目录]
--- getBaiduMusic.py
+++ getBaiduMusic.py
@@ -14,7 +14,7 @@ GLOBAL_VARIABLE = dict()
 HAVEDOWNLOADED = list()
 
 def downloadMusic(musicName, musicId):
-	
+
 	url = 'http://music.baidu.com/song/' + musicId + '/download'
 	print url
 	musicObject = None
@@ -171,29 +171,87 @@ def searchSingerMusic(singerName, album=False):
 					parseMusicList(pageObject.content)
 				start += size
 
+def _getTopTenList(musicName):
+
+	topTenList = []
+	url = 'http://music.baidu.com/search?key=' + musicName
+	response = None
+	try:
+		response = requests.get(url)
+	except Exception:
+		print '_getTopTenList requests.get Error'
+	if response and response.status_code == 200:
+		responseSoup = BeautifulSoup(response.content)
+		songItemList = responseSoup.findAll('div', attrs={'class': 'song-item clearfix'})
+		if songItemList:
+			for songItem in songItemList:
+				titleItem = songItem.find('span', attrs={'class': 'song-title'})
+				a = titleItem.find('a')
+				if titleItem and a:
+					songId = a['href'].split('/')[-1]
+					songTitle = a.text
+					if a.find('em'):
+						songTitle = a.find('em').text
+				singerItem = songItem.find('span', attrs={'class': 'singer'}).find('a')
+				if singerItem and singerItem['title']:
+					singerList = singerItem['title'].split(',')
+
+				albumItem = songItem.find('span', attrs={'class': 'album-title'})
+				if albumItem and albumItem.find('a'):
+					albumName = albumItem.find('a').text
+				topTenList.append([songId, songTitle, singerList, albumName])
+	return topTenList
+
+def _printResultAndGetInput(musicList):
+	for index, music in enumerate(musicList):
+		print index,'\t', music[1], '\t',
+		for singer in music[2]:
+			print singer,
+		print '\t',
+		print music[3]
+	select = input('Please input a number to select music: ')
+	return int(select)
 
 def main():
-	album = False
+
 	dirname = ''
-	# 如果要下载专辑而不是单曲，则加-a选项
-	if '-a' in sys.argv:
-		album = True
 	# 如果加了-d选项，则该选项的后一个列表项即为存储音乐的父目录名
 	if '-d' in sys.argv:
 		dirname = sys.argv[sys.argv.index('-d') + 1]
+	if '-m' in sys.argv:
+		musicName = sys.argv[sys.argv.index('-m') + 1]
+		topTen = _getTopTenList(musicName)
+		userSelect = _printResultAndGetInput(topTen)
+		if userSelect in range(0, len(topTen)):
+			savePath = dirname
+			if not os.path.exists(savePath):
+				os.mkdir(savePath)
+			GLOBAL_VARIABLE['savePath'] = unicode(savePath, DEFAULT_ENCODING)
+			selectedItem = topTen[userSelect]
+			singerStr = '.'.join(selectedItem[2])
+			songTitle = selectedItem[1].replace(' ', '_').replace('\'', '\\\'')
+			downloadMusic(songTitle+'-'+singerStr, selectedItem[0])
+			downloadLRC(songTitle+'-'+singerStr, selectedItem[0])
+	else:
+		album = False
+
+		# 如果要下载专辑而不是单曲，则加-a选项
+		if '-a' in sys.argv:
+			album = True
+
+		# 命令行的第二个参数是歌手的姓名
+		singerName = sys.argv[1]
 		if not dirname.endswith(os.sep):
 			dirname += os.sep
-	# 命令行的第二个参数是歌手的姓名
-	singerName = sys.argv[1]
-	savePath = dirname + singerName.replace(' ', '_')
-	if not os.path.exists(savePath):
-		os.mkdir(savePath)
+		savePath = dirname + singerName.replace(' ', '_')
+		if not os.path.exists(savePath):
+			os.mkdir(savePath)
 
-	GLOBAL_VARIABLE['savePath'] = unicode(savePath, DEFAULT_ENCODING)
-	GLOBAL_VARIABLE['singerName'] = unicode(singerName, DEFAULT_ENCODING)
+		GLOBAL_VARIABLE['savePath'] = unicode(savePath, DEFAULT_ENCODING)
+		GLOBAL_VARIABLE['singerName'] = unicode(singerName, DEFAULT_ENCODING)
 
-	singerName = singerName.decode(DEFAULT_ENCODING).encode('utf-8')
-	searchSingerMusic(singerName, album = album)
+		singerName = singerName.decode(DEFAULT_ENCODING).encode('utf-8')
+		searchSingerMusic(singerName, album = album)
 
 if __name__ == '__main__':
 	main()
