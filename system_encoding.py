95c989a7f2f165b91cd97e9ff1ec9351980a4c18
youngsterxyf <sas.198708@gmail.com>
2013-06-07 14:24:05 +0800

    update

--- system_encoding.py
+++ system_encoding.py
@@ -5,5 +5,5 @@ import platform
 def get_platform_encoding():
     platform_encoding = 'utf-8'
     if platform.system() not in ['Linux', 'Darwin']:
-        platfrom_encoding = 'gbk'
+        platform_encoding = 'gbk'
     return platform_encoding
